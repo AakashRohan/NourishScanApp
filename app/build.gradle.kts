af9ad52982514475d43f90fcd3bf755919f9793d
Aakash Rohra <aakash@Aakashs-MacBook-Air.local>
2024-06-14 12:38:05 +0530

    only one build error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.nourishscanapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.nourishscanapp"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
